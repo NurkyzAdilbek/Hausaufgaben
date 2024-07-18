5440ce83ae52e757560d35fffda1a05ad52bb414
NurkyzAdilbek <nurkyz.adilbekova.kk@gmail.com>
2024-07-18 11:35:45 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,61 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+//alter table movies alter column movie_name set not null;
+//12:18:26
+//insert into movies(movie_name, producers_name, year_of_release) values ('Inception','Emma Thomas, Christopher Nolan','2010-10-09');
+//12:17:16
+//insert into movies(movie_name, producers_name, year_of_release) values ('Inception','Emma Thomas, Christopher Nolan','2010');
+//12:16:39
+//alter table movies rename producer_name to producers_name;
+//12:13:21
+//insert into movies (movie_id, movie_name, producer_name, year_of_release) VALUES ('Avatar',' Jon Landau', 2009);
+//11:56:20
+//insert into movies (movie_id, movie_name, producer_name, year_of_release) VALUES ('Avatar',' Jon Landau', 2009);
+//11:56:16
+//insert into movies(movie_name, producer_name, year_of_release) values ('Avatar',' Jon Landau', 2009);
+     /*   INSERT INTO public.movies ( movie_id) VALUES ( DEFAULT ) returning movie_id;
+        12:20:57
+        SELECT * FROM public.movies ORDER BY movie_id ASC
+        12:20:57
+        SELECT * FROM public.movies ORDER BY movie_id ASC
+        12:20:06
+        select * from movies;
+        12:19:14
+        select from * from movies;
+        12:18:59
+        alter table movies alter column movie_name set not null;
+        12:18:26
+        insert into movies(movie_name, producers_name, year_of_release) values ('Inception','Emma Thomas, Christopher Nolan','2010-10-09');
+        12:17:16
+        insert into movies(movie_name, producers_name, year_of_release) values ('Inception','Emma Thomas, Christopher Nolan','2010');
+        12:16:39
+        alter table movies rename producer_name to producers_name;
+        12:13:21
+        insert into movies (movie_id, movie_name, producer_name, year_of_release) VALUES ('Avatar',' Jon Landau', 2009);
+        11:56:20
+        insert into movies (movie_id, movie_name, producer_name, year_of_release) VALUES ('Avatar',' Jon Landau', 2009);
+        11:56:16
+        insert into movies(movie_name, producer_name, year_of_release) values ('Avatar',' Jon Landau', 2009);
+        11:55:08
+        insert into movies(movie_name, producer_name, year_of_release) values ('Avatar',' Jon Landau', 2009);
+        11:55:07
+        insert into movies(movie_name, producer_name, year_of_release) values ('Avatar',' Jon Landau', 2009);
+        11:55:05
+        insert into movies(movie_name, producer_name, year_of_release) values ('Avatar',' Jon Landau', 2009);
+        11:55:04
+        insert into movies(movie_name, producer_name, year_of_release) values ('Avatar',' Jon Landau', 2009);
+        11:54:38
+        insert into movies(movie_name, producer_name, year_of_release) values ('Avatar',' Jon Landau', 2009);
+        11:54:38
+        insert into movies(movie_name, producer_name, year_of_release) values ('Avatar',' Jon Landau', 2009);
+        11:54:34
+        insert into movies(movie_name, producer_name, year_of_release) values ('Avatar',' Jon Landau', 2009);
+        11:41:25
+        insert into movies(movie_name, producer_name, year_of_release) values ('Avatar',' Jon Landau', 2009);
+        11:41:18*/
+
+    }
+}
